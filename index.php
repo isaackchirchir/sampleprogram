9da94dbc28c05c8c46c260078a9413e2c0bbc446
isaackchirchir <isaackchirchir98@gmail.com>
2019-10-15 18:52:59 -0700

    Added the homepage

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Bootstrap 4 CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css">
+    <title>Home page</title>
+</head>
+<body>
+<div class="container">
+   <div class="row">
+       <div class="col-md-4 offset-md-4 form-div login">
+       <div class="alert alert-success">
+           You are now logged in!
+</div>
+<h3>Welcome ChartSport</h3>
+<a href="#"class="logout">Logout</a>
+
+<div class="alert alert-warning">
+    You need to verify your account to use Chatsport.
+    Sign in to your email account and click on the
+     verrification link we just emailed you at
+     <strong>isaackchirchir98@gmail.com</strong>
+
+</div>
+<button class="btn btn-block btn-lg btn-primary ">I am Verified!</button>
+       </div>
+       </div>
+       </div>
+</body>
+</html>
